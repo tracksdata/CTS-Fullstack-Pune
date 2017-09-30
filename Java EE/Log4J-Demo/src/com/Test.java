31802fdaed0868099c2d3601ed5ba799013d9ed8
richardsforu <praveen.somireddy@gmail.com>
2017-09-30 12:46:19 +0530

    Anser Key Added

---
+++ Java EE/Log4J-Demo/src/com/Test.java
@@ -0,0 +1,11 @@
+package com;
+
+class Test {
+
+	public static void main(String[] args){
+		String test = "This is a test";
+		String[] tokens = test.split("/s");
+		System.out.println(tokens.length);
+		 }
+
+}
