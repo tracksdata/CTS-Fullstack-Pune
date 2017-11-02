e193ab9a2f4bb06bf23fdf6685a3ea9abc7949ca
richardsforu <praveen.somireddy@gmail.com>
2017-11-02 15:49:09 +0530

    jQuery added

--- jQuery/App-1/js/one.js
+++ jQuery/App-1/js/one.js
@@ -1,30 +1,37 @@
 $(document).ready(function(){
-$('.vacation').on('click','button',function(){
-    
-    var vacation=$(this).closest('.vacation');
-    var amount=vacation.data('price');
-    var price = $(`<p> ${amount} </p>`);
-    vacation.append(price);
-    //price.appendTo($('.vacation'))
-    
-    //$(this).closest('div').append(price);
-    $(this).remove();
-})
 
-$('#filters').on('click','.btn-success',function(){
-    console.log('-- on salle');
-    $('.bg-info').removeClass('bg-info');
-    $('.vacation').filter('.onsale').addClass('bg-info');
 
-})
+$('.vacation').on('click','button',function(){  
+ 
+var price=$('<p> From 84683 </p>');
+ 
+var vacation=$(this).closest('.vacation');
 
-$('#filters').on('click','.btn-danger',function(){
-    $('.bg-info').removeClass('bg-info');
-    $('.vacation').filter('.expire').addClass('bg-info');
+var amount=vacation.data('price');
+var price=$(`<p> &#8377; ${amount} </p>`);
+
+//price.appendTo('.vacation');
+price.appendTo(vacation);
+$(this).remove();
 
 })
 
 
+$('#filters').on('click','.onSale-filter',function(){
+   $('.bg-info').removeClass('bg-info');
+   $('.vacation').filter('.onSale').addClass('bg-info');
+
+})
+
+$('#filters').on('click','.expiring-filter',function(){
+   $('.bg-info').removeClass('bg-info');
+   $('.vacation').filter('.expiring').addClass('bg-info'); 
+})
+
+
+$('.confirmation').on('click','button',function(){
+    $(this).closest('.confirmation').find('.tickets').slideToggle();
+})
 
 
 });
