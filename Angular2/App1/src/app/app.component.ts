96759526d05fca6e5470cd03abbfc5c27140fd7d
richardsforu <praveen.somireddy@gmail.com>
2017-11-08 11:51:43 +0530

    Angular is added

---
+++ Angular2/App1/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+
+  template:`
+            <h1> Welcome to Angular2 App</h1>
+          `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
