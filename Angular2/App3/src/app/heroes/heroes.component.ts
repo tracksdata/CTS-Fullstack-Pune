5c9483004622d4cef0984ecf76e2d8fd90626fa6
richardsforu <praveen.somireddy@gmail.com>
2017-11-16 18:16:54 +0530

    Angular Http Spring rest app is added

--- Angular2/App3/src/app/heroes/heroes.component.ts
+++ Angular2/App3/src/app/heroes/heroes.component.ts
@@ -9,12 +9,39 @@ import { HEROES } from '../mock-heros';
 })
 export class HeroesComponent implements OnInit {
 
- heroes=HEROES;
+heroes=HEROES;
+public showData=false;
+course='Select One';
+name='Praveen';
+public subCourses=['-- Select Sub-Course --'];
+
 
 selectedHero: Hero;
+onSelect1(s2:string){
+  this.course=s2;
+  console.log('---- selected course is '+this.course);
+if(this.course==='MSC'){
+  console.log('--- MSC');
+  this.subCourses=new Array();
+  this.subCourses.push('Master in Electronics');
+  this.subCourses.push('Master in Mathematics');
+  this.subCourses.push('Master in Computer Science');
+  this.subCourses.push('Master in Accounts');
+}
+if(this.course=="BSC"){
+  this.subCourses=new Array();
+  console.log('--- BSC');
+  this.subCourses.push('Batcher of Computers');
+  this.subCourses.push('Batcher of Elect');
+  this.subCourses.push('Batcher of Accounts');
+  this.subCourses.push('Batcher of CA');
+}
+
+}
 
 onSelect(hero: Hero):void {
   this.selectedHero = hero;
+
 }
 
   constructor() { }
